0da84f73a2f8698c117f2b7edd6858cd6a580611
kozyrets <54317927+kozyrets@users.noreply.github.com>
2021-01-30 18:22:32 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,100 @@
+const path = require('path');
+const url = require('url');
+const {app, BrowserWindow} = require('electron');
+const electronInstaller = require('electron-winstaller');
+
+if (handleSquirrelEvent(app)) {
+    return;
+}
+
+let win;
+
+function createWindow() {
+	win = new BrowserWindow({
+		width: 800,
+		height: 600,
+		icon: __dirname + "/img/1.png"
+	});
+
+	win.loadURL(url.format({
+		pathname: path.join(__dirname, 'index.html'),
+		protocol: 'file:',
+		slashes: true
+	}));
+
+	// win.webContents.openDevTools();
+
+	win.on('closed', () => {
+		win = null;
+	});
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+	app.quit();
+});
+
+function handleSquirrelEvent(application) {
+    if (process.argv.length === 1) {
+        return false;
+    }
+
+    const ChildProcess = require('child_process');
+    const path = require('path');
+
+    const appFolder = path.resolve(process.execPath, '..');
+    const rootAtomFolder = path.resolve(appFolder, '..');
+    const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
+    const exeName = path.basename(process.execPath);
+
+    const spawn = function(command, args) {
+        let spawnedProcess, error;
+
+        try {
+            spawnedProcess = ChildProcess.spawn(command, args, {
+                detached: true
+            });
+        } catch (error) {}
+
+        return spawnedProcess;
+    };
+
+    const spawnUpdate = function(args) {
+        return spawn(updateDotExe, args);
+    };
+
+    const squirrelEvent = process.argv[1];
+    switch (squirrelEvent) {
+        case '--squirrel-install':
+        case '--squirrel-updated':
+            // Optionally do things such as:
+            // - Add your .exe to the PATH
+            // - Write to the registry for things like file associations and
+            //   explorer context menus
+
+            // Install desktop and start menu shortcuts
+            spawnUpdate(['--createShortcut', exeName]);
+
+            setTimeout(application.quit, 1000);
+            return true;
+
+        case '--squirrel-uninstall':
+            // Undo anything you did in the --squirrel-install and
+            // --squirrel-updated handlers
+
+            // Remove desktop and start menu shortcuts
+            spawnUpdate(['--removeShortcut', exeName]);
+
+            setTimeout(application.quit, 1000);
+            return true;
+
+        case '--squirrel-obsolete':
+            // This is called on the outgoing version of your app before
+            // we update to the new version - it's the opposite of
+            // --squirrel-updated
+
+            application.quit();
+            return true;
+    }
+};
